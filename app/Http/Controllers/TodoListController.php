43b9806814fd2ccb5faac36207c48985017034b8
Kryazhev Kirill <hakkekru@gmail.com>
2022-08-01 11:19:25 +0500

    helpers not working on heroku

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -22,7 +22,7 @@ class TodoListController extends Controller
         if ($validator->fails()) return response()->json(['success' => false, 'data' => $validator->errors()->all()]);
 
         $data = $validator->validated();
-        $data['code'] = makeUniqueCode(array_keys(TodoList::withTrashed()->get(['code'])->groupBy('code')->toArray()), $data['text']);
+        $data['code'] = self::makeUniqueCode(array_keys(TodoList::withTrashed()->get(['code'])->groupBy('code')->toArray()), $data['text']);
 
         $todo = TodoList::create($data);
 
@@ -58,11 +58,57 @@ class TodoListController extends Controller
         }
 
         if (isset($data['text'])) {
-            $data['code'] = makeUniqueCode(array_keys(TodoList::withTrashed()->get(['code'])->groupBy('code')->toArray()), $data['text']);
+            $data['code'] = self::makeUniqueCode(array_keys(TodoList::withTrashed()->get(['code'])->groupBy('code')->toArray()), $data['text']);
         }
 
         if (!empty($data) && !$item->update($data)) return response()->json(['success' => false, 'data' => 'Не удалось обновить элемент. Попробуйте еще раз.']);
 
         return response()->json(['success' => true, 'data' => ['items' => TodoList::all(['text', 'code', 'isUrgent', 'isDone'])->toArray()]]);
+    }
+
+    //методы ниже добавил в отдельный хелпер, локально все работало, но на heroku падала ошибка, что он не знает такие функции, поэтому пришлось перенести сюда...
+    public static function translit(string $value, string $delimiter = '-'): string
+    {
+        $converter = [
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
+            'е' => 'e', 'ё' => 'e', 'ж' => 'zh', 'з' => 'z', 'и' => 'i',
+            'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
+            'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
+            'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
+            'ш' => 'sh', 'щ' => 'sch', 'ь' => '', 'ы' => 'y', 'ъ' => '',
+            'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+        ];
+
+        $value = mb_strtolower($value);
+        $value = strtr($value, $converter);
+        $value = mb_ereg_replace('[^-0-9a-z]', $delimiter, $value);
+        $value = mb_ereg_replace('[-]+', $delimiter, $value);
+        $value = trim($value, $delimiter);
+
+        return $value;
+    }
+
+    public static function makeCode(string $name, string $delimiter = '-', int $maxLength = 10): string
+    {
+        $newCode = self::translit((empty($name) ? 'no-name' : $name), $delimiter);
+        if (mb_strlen($newCode) > $maxLength) $newCode = mb_substr($newCode, 0, $maxLength);
+
+        return $newCode;
+    }
+
+    public static function makeUniqueCode(array $existCodes, string $name, string $delimiter = '-', int $maxTextLength = 10): string
+    {
+        $newCode = self::makeCode($name, $delimiter, $maxTextLength);
+
+        if (!in_array($newCode, $existCodes)) {
+            $newCodeName = $newCode;
+        } else {
+            $kk = 2;
+            do {
+                $newCodeName = $newCode . $delimiter . $kk++;
+            } while (in_array($newCodeName, $existCodes));
+        }
+
+        return $newCodeName;
     }
 }
