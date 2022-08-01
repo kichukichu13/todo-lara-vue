ff1dbb74edaccef7951d9bff92d18adda9695c20
Kryazhev Kirill <hakkekru@gmail.com>
2022-08-01 11:46:38 +0500

    add loader

--- resources/js/components/TodoLists.vue
+++ resources/js/components/TodoLists.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <PageHeader :title='"Todo list"'/>
-        <main>
+
+        <div
+            v-if="firstLoading || loading"
+            class="loader d-flex justify-content-center"
+            :class='{"first-loading": firstLoading}'
+        >
+            <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+        <main v-if="!firstLoading">
             <div class='container'>
                 <div class='section-wrapper'>
                     <div class='row'>
@@ -101,34 +111,36 @@ const items = [
 ]
 
 const addValue = ref('')
+const loading = ref(false)
+const firstLoading = ref(true)
 const itemsCommon = ref([])
 const itemsUrgent = ref([])
 
 onMounted(() => {
-    updateByPromise(todoListGet())
+    updateByPromise(todoListGet({loading: firstLoading}))
 })
 
 const setUndone = (code) => {
-    updateByPromise(todoListEditItem(code, {data: {isDone: false}}))
+    updateByPromise(todoListEditItem(code, {data: {isDone: false}, loading: loading}))
 }
 const setDone = (code) => {
-    updateByPromise(todoListEditItem(code, {data: {isDone: true}}))
+    updateByPromise(todoListEditItem(code, {data: {isDone: true}, loading: loading}))
 }
 const moveToCommon = (code) => {
-    updateByPromise(todoListEditItem(code, {data: {isUrgent: false}}))
+    updateByPromise(todoListEditItem(code, {data: {isUrgent: false}, loading: loading}))
 }
 const moveToUrgent = (code) => {
-    updateByPromise(todoListEditItem(code, {data: {isUrgent: true}}))
+    updateByPromise(todoListEditItem(code, {data: {isUrgent: true}, loading: loading}))
 }
 const editItem = ({code, newText}) => {
     console.log(code, newText, 'editItem')
-    updateByPromise(todoListEditItem(code, {data: {text: newText}}))
+    updateByPromise(todoListEditItem(code, {data: {text: newText}, loading: loading}))
 }
 const deleteItem = (code) => {
-    updateByPromise(todoListDeleteItem(code))
+    updateByPromise(todoListDeleteItem(code, {loading: loading}))
 }
 const addItem = (text) => {
-    const promise = todoListAddItem({data: {text: text}})
+    const promise = todoListAddItem({data: {text: text}, loading: loading})
     updateByPromise(promise)
     promise.then(() => {
         addValue.value = ''
@@ -153,6 +165,17 @@ const updateByPromise = (promiseResult) => {
 </script>
 
 <style scoped>
+.loader {
+    padding: 7rem;
+    position: fixed;
+    z-index: 3;
+    width: 100vw;
+    height: 100vh;
+    background-color: #ffffffad;
+}
+.loader.first-loading {
+    background-color: #fff;
+}
 .footer {
     position: fixed;
     padding: 10px 10px 0 10px;
